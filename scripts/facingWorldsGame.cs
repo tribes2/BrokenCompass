a386e7212a2fd82fe667d80beaa6c19eb049aacb
petrifiedroadkill <2448184+petrifiedroadkill@users.noreply.github.com>
2025-09-15 18:30:17 -0500

    FaceingWorlds Datablock Fix

---
+++ scripts/facingWorldsGame.cs
@@ -0,0 +1,10 @@
+datablock StaticShapeData(faceBox){
+   catagory = "MISC";
+   shapeFile = "faceBox.dts";
+   alwaysAmbient = true;
+   isInvincible = true;
+};
+
+datablock TriggerData(faceDeathTrigger){
+   tickPeriodMS =  1000;
+};
