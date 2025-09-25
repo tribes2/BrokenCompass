d635a851b9b729b520be941d47647fb75c88d021
petrifiedroadkill <2448184+petrifiedroadkill@users.noreply.github.com>
2025-09-25 14:33:15 -0500

    removed the server side stuff

--- scripts/autoexec/dmp2VersionCheck.cs
+++ scripts/autoexec/dmp2VersionCheck.cs
@@ -1,33 +1,10 @@
 // Tells the server what version of the DMP2 pack the client is using
-$DMP2::Version = 0.6;
+$DMP2::Version = 0.2;
 
 // Client Only
 addMessageCallback('MsgDMP2Ver', DMP2Return);
 
 function DMP2Return()
 {
-   commandToServer('ClientDMP2Version',$DMP2::Version);
+   commandToServer('ClientDMP2Version', $DMP2::Version);
 }
- 
-// Server Only
-function serverCmdClientDMP2Version(%client, %version)
-{
-	if(!%client.DMP2Version)
-		%client.DMP2Version = %version;
-}
-
-package DMP2VersionCheck
-{
-
-function GameConnection::onConnect( %client, %name, %raceGender, %skin, %voice, %voicePitch )
-{
-	parent::onConnect( %client, %name, %raceGender, %skin, %voice, %voicePitch );
-	
-	messageClient(%client, 'MsgDMP2Ver');
-}
-
-};
-
-// Prevent package from being activated if it is already
-if (!isActivePackage(DMP2VersionCheck))
-	activatePackage(DMP2VersionCheck);
